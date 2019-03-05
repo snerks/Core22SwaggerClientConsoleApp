233174b593b25975096948a34ab9f4c0dc9f5cc3
snerks <martin.github@outlook.com>
2019-03-05 18:49:24 +0000

    Add project files.

---
+++ Core22SwaggerClientConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core22SwaggerClientConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
